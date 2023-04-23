83e9055bfe5edd6e7c4ebc3cff4e6dbf522c7698
x921682 <81702950+x921682@users.noreply.github.com>
2023-04-24 02:52:17 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <link rel="stylesheet" type="text/css" href="homepage.css">
+    <head>
+        <meta charset="UTF-8">
+        <title>Main Page</title>
+        <style>
+                    fieldset {
+                margin: 0 auto;
+                float: center;
+                background-color: #faf2a0;
+            }
+          </style>
+
+    </head>
+    <body>
+        
+     <br><br>   
+   <fieldset style="width:20%;  border: 2px solid blue;">
+  <h2 style="text-align:center;">Main Page</h2>
+  <hr style="height:1px;width:95%; background:blue;" >
+  
+  <br>
+
+  <p style="text-align:center;"><a href="login.php">LOGIN</a></p> <br>
+  <p style="text-align:center;"><a href="itempage.php">ITEMPAGE</a></p> <br><br>
+         </fieldset>
+    </body>
+</html>
