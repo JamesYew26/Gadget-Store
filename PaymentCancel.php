fe884a3225cbd89cf54c71cbf1499f2c45327f07
youngjuin1234 <youngjuin1234@gmail.com>
2023-04-24 01:43:56 +0800

    Payment
    Payment Successful AND Payment Cancel Page

---
+++ PaymentCancel.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Payment Cancel</title>
+       <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+<main>
+    <br><br>
+    <center>  
+        <div><img src="cancel.png" alt="cancel" style="width:300px;height:300px;"></div>
+        <h1 class="font-title">Payment Cancelled</h1>
+        <p>Payment has been cancel for some reason.</p>
+        <div class="Homebuttons">
+        <button onclick="window.location.href='index.php'">Return to Homepage</button>
+        </div>
+   </center>
+</main>
+</body>
+</html>
---
+++ PaymentSuccessful.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Payment Successful</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+<main>
+    <br><br>
+    <center>
+        <div><img src="successful.png" alt="success" style="width:300px;height:300px;"></div>
+        <h1 class="font-title">Payment Done Successfully</h1>
+            <p>Your Order Has Been Placed, Thank you for ordering with us!</p> 
+       
+        <div class="Homebuttons">
+            <button onclick="window.location.href='index.php'">Return to Homepage</button> <br>
+        </div>
+            
+            <div class="OrderButton">
+        <button onclick="window.location.href='Order.php'">View Order & Receipt</button>
+        </div>
+ </center>       
+</main>
+    </body>
+</html>
