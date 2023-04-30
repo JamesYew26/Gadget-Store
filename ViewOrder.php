d6c09ede236a1b39c53135a7dbcab9304187ab5c
youngjuin1234 <youngjuin1234@gmail.com>
2023-05-01 02:52:32 +0800

    update ViewOrder.php

--- ViewOrder.php
+++ ViewOrder.php
@@ -1,13 +1,16 @@
 <?php 
-$servername= "localhost";
-$username= "username";
-$password = "password";
-$db_name = "GadgetStore";
+$sname= "localhost";
+$unmae= "root";
+$password = "";
 
-$conn = mysqli_connect($servername, $username, $password, $db_name);
+$db_name = "shoppingcart";
 
+$conn = new mysqli($sname, $unmae, $password, $db_name);
 
+$sql ="SELECT userID , productID, name,img, quantity, rrp, price, date_ordered   FROM `userorder` WHERE `userID`=1"; 
+$result = $conn->query($sql);
 ?>
+
 <!DOCTYPE html>
 
 <html>
@@ -23,21 +26,10 @@ $conn = mysqli_connect($servername, $username, $password, $db_name);
     <center>
         <br><br>
         <h2>Customer Ordered</h2>
-        <?php
-     
-$query = $mysqli->query('SELECT *  FROM `userorder` WHERE `userID`=1');
-
-
-/*foreach ($sql as $user)
-{
-    echo $user['userID'].' - '.$user['productID'].' - '.$user['name'].' - '.$user['img'].' - '.$user['quantity'].' - '.$user['rrp'].' - '.$user['price'].' - '.$user['date_ordered'].'<br>';
-
-} */ 
-?>
-
-<table border ="1" cellspacing="0" cellpadding="10">
-    <thead>
-            <tr>
+ <br><br>
+ <div class="container" style="text-align: center">
+<table class="table table-hover">
+            <tr class="table-primary">
                 <th>Customer ID</th>
                 <th>Product ID</th>
                 <th>Product Name</th>
@@ -47,30 +39,27 @@ $query = $mysqli->query('SELECT *  FROM `userorder` WHERE `userID`=1');
                 <th>Price</th>
                 <th>Date Ordered</th>
             </tr>
-          </thead>   
-          
-           </tbody> 
-              <tr>
-	 <?php foreach ($query as $data){ ?>
-                          <td><?php $data['userID']?></td>
-                          <td><?php $data['productID']?></td>
-                          <td><?php $data['name']?></td>
-                          <td><?php $data['img']?></td>
-                          <td><?php $data['quantity']?></td>
-                          <td><?php $data['rrp']?></td>
-                          <td><?php $data['price']?></td>
-                          <td><?php $data['date_ordered']?></td>
-             </tr>
 
-  	     <?php } ?>    
-          </tbody>       
+  <?php
+if ($result -> num_rows>0){
+while ($row = $result->fetch_assoc()){
+    echo "<tr><td>".$row["userID"]."</td><td>".$row["productID"]."</td><td>".$row["name"]."</td><td><img idth='50' height='50' src='imgs/".$row["img"]."'></td><td>".$row["quantity"]."</td><td>".$row["rrp"]."</td><td>".$row["price"]."</td><td>".$row["date_ordered"]."</td></tr>";
+}
+      echo "</table>";
+  }
+  else {
+      echo "No Place any order yet.";
+  }
+      $conn->close();        
+   ?>
+                 
 </table>
-               
+ </div>  
           <br><button  type="button" class="btn btn-primary" onclick="window.print()" > <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
               <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
               <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
               </svg>
-              <i class="bi bi-printer-fill">Print Order</i></button>
+              <i class="bi bi-printer-fill">Print Receipt</i></button>
 
 </center>
 </body>
