ddf4eb9de5e60efaba40125c701c8fe61e5afb7c
youngjuin1234 <youngjuin1234@gmail.com>
2023-05-02 18:04:50 +0800

    update

--- itempage.php
+++ itempage.php
@@ -7,8 +7,8 @@
 
 include 'functions.php';
 $pdo = pdo_connect_mysql();
-// Get the 4 most recently added products
-$stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT 4');
+// Get the 12 most recently added products
+$stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT 12');
 $stmt->execute();
 $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -23,7 +23,7 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <div class="products">
         <?php foreach ($recently_added_products as $product): ?>
         <a href="index.php?page=product&id=<?=$product['id']?>" class="product">
-            <img src="imgs/<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
+            <img src="imgs\<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
             <span class="name"><?=$product['name']?></span>
             <span class="price">
                 &#82;&#77;<?=$product['price']?>
