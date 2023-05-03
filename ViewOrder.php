273498676cf25d821cb054b6870a8f64e2675d45
youngjuin1234 <youngjuin1234@gmail.com>
2023-05-03 17:00:01 +0800

    update ViewOrder.php

--- ViewOrder.php
+++ ViewOrder.php
@@ -6,7 +6,7 @@ $db_name = "GadgetStore";
 
 $conn = mysqli_connect($servername, $username, $password, $db_name);
 
-$sql ="SELECT userID , productID, name,img, quantity, rrp, price, date_ordered,invoiceID   FROM `userorder` WHERE `userID`=1"; 
+$sql ="SELECT userID , productID, name,img, quantity, rrp, price, date_ordered,invoiceID   FROM `userorder` WHERE `userID`=1001"; 
 $result = $conn->query($sql);
 ?>
 
