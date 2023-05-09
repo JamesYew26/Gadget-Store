5dac64057e1454a5819d2413598f974faa1a8b55
youngjuin1234 <youngjuin1234@gmail.com>
2023-05-10 07:52:50 +0800

    Reset password update

--- resetpasscheck.php
+++ resetpasscheck.php
@@ -12,12 +12,10 @@ if (isset($_POST['password']) && isset($_POST['repassword']) && isset($_POST['em
         return $data;
     }
 
-    
     $password = validate($_POST['password']);
     $repassword = validate($_POST['repassword']);
     $email = validate($_POST['email']);
 
-    
     if (empty($password)) {
         header("Location: reset_pass.php?error=Password is required");
         exit();
@@ -27,33 +25,16 @@ if (isset($_POST['password']) && isset($_POST['repassword']) && isset($_POST['em
     } else if (empty($email)) {
         header("Location: reset_pass.php?error=Email is required");
         exit();
-    }
-    elseif ($password !== $repassword) {
-        header("Location: reset_pass.php?error=Please make your passwords are matched");
+    } elseif ($password !== $repassword) {
+        header("Location: reset_pass.php?error=Please make sure your passwords are matched");
         exit();
     } else {
         // hashing the password
         $password = md5($password);
 
-        $sql = "UPDATE `credential` SET password = '$password' WHERE email = '$email';";
-                $result = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['email'] === $email && $row['password'] === $password) {
-            	$_SESSION['email'] = $row['email'];
-                
-                
-                header("Location: index.php?page=login");
-                exit();
-            } else {
-                header("Location: reset_pass.php?error=Please make your passwords are matched");
-                exit();
-            }
-        } else {
-            header("Location: reset_pass?error=Please make your passwords are matched");
-            exit();
-        }
+        mysqli_query($conn, "UPDATE `credential` SET password = '$password' WHERE email='$email';");
+        header("Location: index.php?page=login");
+        exit();
     }
 } else {
     header("Location: index.php?page=login");
