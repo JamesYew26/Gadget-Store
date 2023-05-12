836e5758cb296ac74642a8e4d57f3a977751d2a2
youngjuin1234 <youngjuin1234@gmail.com>
2023-05-12 08:13:54 +0800

    adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -114,7 +114,7 @@ include 'index.html';
 			<!--Submit DIV-->
 			<button type="submit" class="btn btn-primary">Login</button>
 			<div class="mt-3"><a href="index.php?page=adminregister" class="ca">Create Admin account</a>
- <div class="mt-3"><a href="index.php?page=login" class="ca">Back to User Login</a>
+                     <div class="mt-3"><a href="index.php?page=login" class="ca">Back to User Login</a>
 		</form>
 	</center>
 
